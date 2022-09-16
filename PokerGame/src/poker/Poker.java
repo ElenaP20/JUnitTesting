9ec6aa35a2619a23b4973229ecaea823431c279b
Eli <Eli@172.28.150.208>
2022-09-16 13:52:22 +0800

    Poker Game initial version

---
+++ PokerGame/src/poker/Poker.java
@@ -0,0 +1,52 @@
+package poker;
+
+public class Poker {
+	// Precondition: A hand of n cards have been sorted by card number.
+	public boolean isFullHouse(String cards[], int n) {
+		return isThreeOfaKind(cards, n) && isTwoPairs(cards, n) &! isFourOfKind(cards,n);
+	}
+
+	// Precondition: A hand of n cards have been sorted by card number.
+	public boolean isThreeOfaKind(String cards[], int n) {
+		for (int i=0; i<n-2; i++) {
+			if (cards[i].charAt(1) == cards[i+1].charAt(1) &&
+					cards[i+1].charAt(1) == cards[i+2].charAt(1))
+					return true;
+		}
+
+		return false;
+	}
+
+	// Precondition: A hand of n cards have been sorted by card number.
+	public boolean isTwoPairs(String cards[], int n) {
+		int count = 0;
+		for (int i=0; i<n-1; i++) {
+			if (cards[i].charAt(1) == cards[i+1].charAt(1)) {
+				count++;
+				i++;
+			}
+		}
+		if (count == 2)
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean isFourOfKind(String cards[], int n) {
+		for (int i=0; i<n-1; i++) {
+			if (cards[i].charAt(1) == cards[i+1].charAt(1) && cards[i+1].charAt(1) == cards[i+2].charAt(1) && cards[i+2].charAt(1)== cards[i+3].charAt(1) ) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	public static void main(String args[]) {
+		System.out.println(new Poker().isFullHouse(
+			new String[] {"C2", "D2", "H2", "S3", "S4"}, 5));
+		System.out.println(new Poker().isFourOfKind(
+				new String[] {"C6", "D6", "H6", "S6", "D7"}, 5));
+	}
+}
+// end of Poker.java
